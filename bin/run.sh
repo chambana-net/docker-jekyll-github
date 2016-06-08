2f1f5a4ae33b8a2beb09333fc702fef653cb3474
Josh King <jking@chambana.net>
2016-06-08 14:09:11 -0400

    Really clear out directory, fix typo.

--- bin/run.sh
+++ bin/run.sh
@@ -13,13 +13,15 @@ SUBDIR=${SUBDIR:-/}
 GITHUB_BRANCH=${GITHUB_BRANCH:-"master"}
 
 MSG "Cloning repository..."
-rm -rf /srv/www/*
+rm -rf /srv/www
+mkdir /srv/www
 git clone -b ${GITHUB_BRANCH} --single-branch https://github.com/${GITHUB_USER}/${GITHUB_REPO} /srv/www
 [[ $? -eq 0 ]] || { ERR "Failed to clone repository, aborting."; exit 1; }
 [[ -d /srv/www/${SUBDIR} ]] || { ERR "Subdirectory $SUBDIR does not exist, aborting."; exit 1; }
 
 MSG "Serving site..."
 cd /srv/www/${SUBDIR}
-[[ -e /srv/www/${SUBDIR}/Gemfile ]] && { bundle install } 
+[[ -e /srv/www/${SUBDIR}/Gemfile ]] && bundle install 
 
 exec "$@"
+
