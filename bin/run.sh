61455ee7a47a9dc886e6f183d88ebac7c5f5ee53
Josh King <jking@chambana.net>
2016-06-08 15:59:53 -0400

    Only clone the repo if it doesn't exist already, otherwise just update it.

--- bin/run.sh
+++ bin/run.sh
@@ -12,11 +12,21 @@ CHECK_VAR GITHUB_REPO
 SUBDIR=${SUBDIR:-/}
 GITHUB_BRANCH=${GITHUB_BRANCH:-"master"}
 
-MSG "Cloning repository..."
-rm -rf /srv/www
-mkdir /srv/www
-git clone -b ${GITHUB_BRANCH} --single-branch https://github.com/${GITHUB_USER}/${GITHUB_REPO} /srv/www
-[[ $? -eq 0 ]] || { ERR "Failed to clone repository, aborting."; exit 1; }
+if [[ -d /srv/www/.git ]]; then
+	MSG "Updating respository..."
+	cd /srv/www
+	git checkout ${GITHUB_BRANCH}
+	[[ $? -eq 0 ]] || { ERR "Branch ${GITHUB_BRANCH} doesn't exist, aborting."; exit 1; }
+	git fetch --all
+	git reset --hard origin/${GITHUB_BRANCH}
+else
+	MSG "Cloning repository..."
+	rm -rf /srv/www
+	mkdir /srv/www
+	git clone -b ${GITHUB_BRANCH} --single-branch https://github.com/${GITHUB_USER}/${GITHUB_REPO} /srv/www
+	[[ $? -eq 0 ]] || { ERR "Failed to clone repository, aborting."; exit 1; }
+fi
+
 [[ -d /srv/www/${SUBDIR} ]] || { ERR "Subdirectory $SUBDIR does not exist, aborting."; exit 1; }
 
 MSG "Serving site..."
